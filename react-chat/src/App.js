c0dedf12494437084b26ce012e1cf52549a94274
kgn99 <y3ef9484@gmail.com>
2022-10-31 14:28:52 +0900

    feat : React pusher 및 채팅 구현

--- react-chat/src/App.js
+++ react-chat/src/App.js
@@ -1,30 +1,72 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
+import Pusher from "pusher-js";
 
 function App() {
   const [username, setUsername] = useState("username");
+  const [messages, setMessages] = useState([]);
+  const [message, setMessage] = useState("");
+  let allMessages = [];
+
+  useEffect(() => {
+    Pusher.logToConsole = true;
+
+    const pusher = new Pusher("355b0c485bcc63a17228", {
+      cluster: "ap3",
+    });
+
+    const channel = pusher.subscribe("chat");
+    channel.bind("message", function (data) {
+      allMessages.push(data);
+      setMessages(allMessages);
+    });
+  }, []);
+
+  const submit = async (e) => {
+    e.preventDefault();
+
+    await fetch("http://localhost:8000/api/messages", {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({
+        username,
+        message,
+      }),
+    });
+
+    setMessage("");
+  };
+
   return (
     <div className="container">
-      <div class="d-flex flex-column align-items-stretch flex-shrink-0 bg-white">
-        <div class="d-flex align-items-center flex-shrink-0 p-3 link-dark text-decoration-none border-bottom">
+      <div className="d-flex flex-column align-items-stretch flex-shrink-0 bg-white">
+        <div className="d-flex align-items-center flex-shrink-0 p-3 link-dark text-decoration-none border-bottom">
           <input
-            class="fs-5 fw-semibold"
+            className="fs-5 fw-semibold"
             value={username}
             onChange={(e) => setUsername(e.target.value)}
           />
         </div>
-        <div class="list-group list-group-flush border-bottom scrollarea">
-          <div class="list-group-item list-group-item-action py-3 lh-sm">
-            <div class="d-flex w-100 align-items-center justify-content-between">
-              <strong class="mb-1">List group item heading</strong>
-              <small class="text-muted">Tues</small>
-            </div>
-            <div class="col-10 mb-1 small">
-              Some placeholder content in a paragraph below the heading and
-              date.
-            </div>
-          </div>
+        <div className="list-group list-group-flush border-bottom scrollarea">
+          {messages.map((message) => {
+            return (
+              <div className="list-group-item list-group-item-action py-3 lh-tight">
+                <div className="d-flex w-100 align-items-center justify-content-between">
+                  <strong className="mb-1">{message.username}</strong>
+                </div>
+                <div className="col-10 mb-1 small">{message.message}</div>
+              </div>
+            );
+          })}
         </div>
       </div>
+      <form onSubmit={(e) => submit(e)}>
+        <input
+          className="form-control"
+          placeholder="Write a message"
+          value={message}
+          onChange={(e) => setMessage(e.target.value)}
+        />
+      </form>
     </div>
   );
 }
