0aec779dd7de65f054728aece6f70bafbc15f1c9
kgn99 <y3ef9484@gmail.com>
2022-10-31 13:46:50 +0900

    feat : Pusher 추가

---
+++ chat/pusher.py
@@ -0,0 +1,10 @@
+import pusher
+
+pusher_client = pusher.Pusher(
+  app_id='1499356',
+  key='355b0c485bcc63a17228',
+  secret='757eaecf5d11f0edf86c',
+  cluster='ap3',
+  ssl=True
+)
+
--- chat/views.py
+++ chat/views.py
@@ -1,6 +1,13 @@
 from django.shortcuts import render
+from rest_framework.response import Response
 from rest_framework.views import APIView
+from .pusher import pusher_client
 # Create your views here.
 class MessageAPIView(APIView):
     def post(self, request):
-        pass
+        pusher_client.trigger('chat', 'message', {
+            'username':request.data['username'],
+            'message':request.data['message'],
+        })
+
+        return Response([])
